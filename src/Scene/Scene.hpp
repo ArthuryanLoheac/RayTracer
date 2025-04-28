9cc7c4e86864d9bd0481a429fb62198af75d7422
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-04-28 10:21:11 +0200

    ✨ Change lights vector to use unique_ptr for better memory management

--- src/Scene/Scene.hpp
+++ src/Scene/Scene.hpp
@@ -8,7 +8,7 @@
 namespace RayTracer {
 class Scene {
  public:
-    std::vector<I_Lights> lights;
+    std::vector<std::unique_ptr<I_Lights>> lights;
     std::unique_ptr<I_Primitive> ObjectHead;
 };
 }  // namespace RayTracer
