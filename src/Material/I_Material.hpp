05a867962964f5e4c8c2d545093560bf5db0651d
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-04-28 10:11:11 +0200

    ✨ Add I_Lights class

--- src/Material/I_Material.hpp
+++ src/Material/I_Material.hpp
@@ -4,6 +4,7 @@
 namespace RayTracer {
 class I_Material {
  public:
+    virtual ~I_Material() = default;
     virtual void Init() = 0;
     virtual sf::Color getColorAt(int x, int y) = 0;
 };
--- src/Primitive/I_Primitive.hpp
+++ src/Primitive/I_Primitive.hpp
@@ -7,18 +7,14 @@
 namespace RayTracer {
 class I_Primitive {
  public:
-    I_Primitive() = default;
+    virtual ~I_Primitive() = default;
     virtual void Init() = 0;
     virtual std::unique_ptr<I_Primitive> getParent() = 0;
     virtual void setParent(std::unique_ptr<I_Primitive> parent) = 0;
     virtual I_Material getMaterial() = 0;
-    virtual void setMaterial(I_Material material) = 0;
     virtual Point3D getPosition() = 0;
-    virtual void setPosition(Point3D position) = 0;
     virtual Point3D getRotation() = 0;
-    virtual void setRotation(Point3D rotation) = 0;
     virtual Point3D getScale() = 0;
-    virtual void setScale(Point3D scale) = 0;
 };
 }  // namespace RayTracer
 
