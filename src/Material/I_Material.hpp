1471f181a3824807c66aacac57fc4f83497c1098
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-04-28 10:06:36 +0200

    ✨ Add I_Material class

--- src/Material/I_Material.hpp
+++ src/Material/I_Material.hpp
@@ -1,9 +1,12 @@
-#pragma once 
+#pragma once
+#include <SFML/Graphics.hpp>
 
 namespace RayTracer {
 class I_Material {
  public:
-    I_Material(/* args */);
-    ~I_Material();
+    virtual void Init() = 0;
+    virtual sf::Color getColorAt(int x, int y) = 0;
 };
 } // namespace RayTracer
+
+// extern "C" std::unique_ptr<RayTracer::I_Material> getMaterial();
