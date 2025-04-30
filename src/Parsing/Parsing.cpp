09c6e1ec27eac127b2096fc749f1b35b6736b6e9
mathys-f <mathys.fillion@epitech.eu>
2025-04-30 11:42:29 +0200

    ✨ Implement Parsing class for argument handling and error

---
+++ src/Parsing/Parsing.cpp
@@ -0,0 +1,29 @@
+#include <libconfig.h++>
+#include <iostream>
+
+#include "Parsing.hpp"
+#include "../Scene/Scene.hpp"
+
+namespace RayTracer {
+
+void Parsing::parseArgs(int argc, char **argv) {
+    if (argc < 2)
+        throw ParsingError("Missing scene file argument.");
+    if (argc > 2)
+        throw ParsingError("Too many arguments.");
+    if (std::string(argv[1]) == "-help") {
+        std::cout << "USAGE: ./rayTracer <SCENE_FILE>" << std::endl;
+        std::cout << "  SCENE_FILE: scene configuration" << std::endl;
+        exit(0);
+    }
+    sceneFile = argv[1];
+}
+
+Parsing::ParsingError::ParsingError(const std::string &message)
+    : message("ParsingError: " + message) {}
+
+const char *Parsing::ParsingError::what() const noexcept {
+    return message.c_str();
+}
+
+} // namespace RayTracer
