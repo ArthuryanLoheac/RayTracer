511d0d98361a39cb219a200e929b13200a81ecbf
Mathys Fillion <146719174+mathys-f@users.noreply.github.com>
2025-04-30 16:18:23 +0200

    ✨ Parse arguments and open config file (#25)
    * 💚 Fix trailing whitespace in GitHub Actions workflow file
    
    * 🔧 Add config++ library to Makefile flags
    
    * 🙈 Update .gitignore to include *cfg patterns
    
    * ✨ Add Parsing class for scene file handling and error management
    
    * ✨ Implement Parsing class for argument handling and error
    
    * ✨ Add parseSceneFile method to Parsing class for scene file processing
    
    * ✨ Add parseSceneFile implementation to Parsing class for scene file reading and error handling
    
    * ♻️ Refactor main function to integrate Parsing and Scene classes
    
    * 🎨 Fix coding style
    
    * 💚 Add installation step for libconfig++ in GitHub Actions workflow

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@
 #include "Interfaces/Primitive/I_Primitive.hpp"
 #include "Interfaces/Light/I_Light.hpp"
 #include "dlLoader/dlLoader.hpp"
+#include "Parsing/Parsing.hpp"
+#include "Scene/Scene.hpp"
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
@@ -29,7 +31,7 @@ static void setupAndRun(sf::RenderWindow &window, sf::Image &image) {
     displayImage(window, image);
 }
 
-int main() {
+void testMain() {
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Ray Tracer");
     sf::Image image;
     image.create(WIDTH, HEIGHT, sf::Color::Black);
@@ -38,7 +40,20 @@ int main() {
         setupAndRun(window, image);
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
+    }
+}
+
+int main(int argc, char **argv) {
+    RayTracer::Parsing parser;
+    RayTracer::Scene scene;
+
+    try {
+        parser.parseArgs(argc, argv);
+        scene = parser.parseSceneFile();
+    } catch (const RayTracer::Parsing::ParsingError &e) {
+        std::cerr << e.what() << std::endl;
         return 84;
     }
+    testMain();
     return 0;
 }
---
+++ src/Parsing/Parsing.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <string>
+#include <libconfig.h++>
+
+#include "Parsing/Parsing.hpp"
+#include "Scene/Scene.hpp"
+
+namespace RayTracer {
+
+void Parsing::parseArgs(int argc, char **argv) {
+    if (argc < 2)
+        throw ParsingError("Missing scene file argument.");
+    if (argc > 2)
+        throw ParsingError("Too many arguments.");
+    if (std::string(argv[1]) == "-help") {
+        std::cout << "USAGE: ./rayTracer <SCENE_FILE>" << std::endl;
+        std::cout << "  SCENE_FILE: scene configuration" << std::endl;
+        exit(0);
+    }
+    sceneFile = argv[1];
+}
+
+Scene Parsing::parseSceneFile() {
+    RayTracer::Scene scene;
+    libconfig::Config cfg;
+
+    try {
+        cfg.readFile(sceneFile.c_str());
+    } catch (const libconfig::FileIOException &fioex) {
+        throw ParsingError("I/O error while reading file.");
+    } catch (const libconfig::ConfigException &ex) {
+        throw ParsingError("Error in configuration file.");
+    }
+    return scene;
+}
+
+Parsing::ParsingError::ParsingError(const std::string &message)
+: message("ParsingError: " + message) {}
+
+const char *Parsing::ParsingError::what() const noexcept {
+    return message.c_str();
+}
+
+}  // namespace RayTracer
