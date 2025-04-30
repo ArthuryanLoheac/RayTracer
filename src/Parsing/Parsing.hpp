474561564183436732c26e68f215e21b3f7e0deb
mathys-f <mathys.fillion@epitech.eu>
2025-04-30 11:41:56 +0200

    ✨ Add Parsing class for scene file handling and error management

---
+++ src/Parsing/Parsing.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "Scene/Scene.hpp"
+
+namespace RayTracer {
+class Parsing {
+ private:
+     std::string sceneFile;
+
+ public:
+     void parseArgs(int argc, char **argv);
+
+     class ParsingError : public std::exception {
+      private:
+          std::string message;
+
+      public:
+          explicit ParsingError(const std::string &message);
+          const char *what() const noexcept override;
+    };
+};
+}  // namespace RayTracer
