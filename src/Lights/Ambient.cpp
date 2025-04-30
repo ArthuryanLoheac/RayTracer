0dd7065673882d67ce2ee14fa53fa1d6ec6b999e
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-04-30 16:18:52 +0200

    🚨 Fix compilation unused intersection in Ambient light

--- src/Lights/Ambient.cpp
+++ src/Lights/Ambient.cpp
@@ -24,5 +24,6 @@ float Ambient::getLuminescence(float distance, float angle) {
 
 bool Ambient::hits(RayTracer::Ray ray, RayTracer::Point3D &intersection) {
     (void) ray;
+    (void) intersection;
     return false;
 }
