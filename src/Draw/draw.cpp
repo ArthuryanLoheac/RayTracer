31c73d70f95519cf81ff430ad1a4c848b3c7c97e
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-05-01 13:47:08 +0200

    ✨  Refactor structure for hit detection

--- src/Draw/draw.cpp
+++ src/Draw/draw.cpp
@@ -37,18 +37,30 @@ void displayImage(sf::RenderWindow &window, sf::Image &image) {
     }
 }
 
+static double getLuminescence(RayTracer::Point3D &intersection,
+    std::unique_ptr<Light> &Light) {
+    double distance = intersection.distance(Light->getPosition());
+    double angle = 0;
+
+    return Light->getLuminescence(distance, angle);
+}
+
 static void hit(sf::Image &image, int i, int j,
     std::unique_ptr<Prim> &s, RayTracer::Point3D &intersection,
     std::unique_ptr<Light> &Light) {
     try {
+        // Get base colors
         sf::Color origin = image.getPixel(i, j);
         sf::Color c = s->getMaterial()->getColorAt(i, j);
-        double distance = intersection.distance(Light->getPosition());
-        double angle = 0;
-        double luminescence = Light->getLuminescence(distance, angle);
-        int r = std::min(255, static_cast<int>(c.r * luminescence));
-        int g = std::min(255, static_cast<int>(c.g * luminescence));
-        int b = std::min(255, static_cast<int>(c.b * luminescence));
+
+        double luminescence = getLuminescence(intersection, Light);
+
+        // Edit color with luminescence values
+        int r = static_cast<int>(c.r * luminescence);
+        int g = static_cast<int>(c.g * luminescence);
+        int b = static_cast<int>(c.b * luminescence);
+
+        // Edit color with transparency values
         float percentA = 255 / c.a;
         c = sf::Color(  (r * percentA) + (origin.r * (1 - percentA)),
                         (g * percentA) + (origin.g * (1 - percentA)),
