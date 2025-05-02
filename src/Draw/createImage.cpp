b077c850628203c7a9504091bb60c2aab32ffcea
nathan bezard <nathan.bezard@epitech.eu>
2025-05-02 14:47:33 +0200

    🩹 Fix random generator to generate different string each runtime

--- src/Draw/createImage.cpp
+++ src/Draw/createImage.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <string>
+#include <random>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
@@ -27,17 +28,18 @@ static void createPPMFile(const sf::Image& image,
     out.close();
 }
 
-static std::string randomString(const int len) {
-    static const char alphanum[] =
-        "0123456789"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-    std::string tmp_s;
-
-    tmp_s.reserve(len);
-    for (int i = 0; i < len; ++i)
-        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
-    return ("renders/screenshot" + tmp_s + ".ppm");
+std::string randomString(int max_length) {
+    std::string characters =
+        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    std::random_device rd;
+    std::mt19937 engine(rd());
+    std::uniform_int_distribution<> dist(0, characters.size()-1);
+    std::string ret = "";
+    for (int i = 0; i < max_length; i++) {
+        int random_index = dist(engine);
+        ret += characters[random_index];
+    }
+    return ("renders/screenshot" + ret + ".ppm");
 }
 
 void renderAtClosing(sf::RenderWindow &window) {
