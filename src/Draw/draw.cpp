e022a950ea809ebc969e6eaa8e59a970e5b75fed
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-05-08 11:15:42 +0200

    🎨 Fix coding style

--- src/Draw/draw.cpp
+++ src/Draw/draw.cpp
@@ -65,7 +65,7 @@ float &minRayLen) {
         // Get base colors
         sf::Color origin = image.getPixel(i, j);
         sf::Color c = s->getMaterial()->getColorAt(i, j);
-        sf::Vector3f cLight = sf::Vector3f(0,0,0);
+        sf::Vector3f cLight = sf::Vector3f(0, 0, 0);
 
         computeLuminescence(intersection, s, cLight);
 
