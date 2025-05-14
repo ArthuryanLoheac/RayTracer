2ba48c0f27d17498740e582ee007ffac9ae77d13
mathys-f <mathys.fillion@epitech.eu>
2025-05-14 07:00:15 +0200

    ✨ Update PrimNone initialization to accept settings via a parameter

--- src/Primitive/PrimNone.cpp
+++ src/Primitive/PrimNone.cpp
@@ -10,7 +10,8 @@ extern "C" std::unique_ptr<RayTracer::I_Primitive> getPrimitive() {
 }
 
 PrimNone::PrimNone() {
-    Init();
+    std::unordered_map<std::string, std::any> settings;
+    Init(settings);
 }
 
 bool PrimNone::hits(RayTracer::Ray ray, RayTracer::Point3D &intersection) {
@@ -24,7 +25,8 @@ RayTracer::Vector3D PrimNone::getNormalAt(RayTracer::Point3D point) {
     return RayTracer::Vector3D(0, 0, 0);
 }
 
-void PrimNone::Init() {
+void PrimNone::Init(std::unordered_map<std::string, std::any> &settings) {
+    (void)settings;
     position = RayTracer::Point3D(0, 0, 0);
     radius = 0.f;
     material = nullptr;
