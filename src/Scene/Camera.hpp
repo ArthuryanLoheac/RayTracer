aef9f7586d184d710ead6d5ad2275e9cb074ac12
mathys-f <mathys.fillion@epitech.eu>
2025-05-15 19:35:16 +0200

    ✨ Implement camera initialization with dynamic settings for width, height, FOV, position, and rotation

--- src/Scene/Camera.hpp
+++ src/Scene/Camera.hpp
@@ -1,4 +1,8 @@
 #pragma once
+#include <any>
+#include <string>
+#include <unordered_map>
+
 #include "3dDatas/Point3D.hpp"
 #include "3dDatas/Vector3D.hpp"
 #include "3dDatas/Ray.hpp"
@@ -9,8 +13,6 @@ namespace RayTracer {
 class Camera {
  private:
     double aspect_ratio;
-    int image_width;
-    int image_height;
     double vfov;  // vertical field of view
     Point3D lookingAt;
     Matrix4x4 camera_to_world;  // 4x4 transformation matrix
@@ -21,10 +23,14 @@ class Camera {
     }
     Point3D origin;
     Rectangle3D screen;
+    Vector3D rotation;
+    int image_width;
+    int image_height;
 
     Camera();  // Default constructor
     Ray ray(double u, double v);
 
+    void Init(std::unordered_map<std::string, std::any> &settings);
     void rotate(double angleX, double angleY, double angleZ);
 };
 }  // namespace RayTracer
